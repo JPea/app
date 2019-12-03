d1cf8b7179a09a03ade428fb2bbf79dc8b124c4b
Glitch (eleventy-on-glitch) <none>
2019-12-03 23:07:53 +0000

    🌿🏈 Checkpoint ./src/_includes/layouts/default.njk:463535/73 ./.eleventy.js:463535/181 ./src/css/style.css:463535/62:8816846/92 ./watch.json:463535/2

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@
 module.exports = function(eleventyConfig) {
   
   // eleventyConfig.addFilter( "myFilter", function() {});
+  
+  // copy the css folder in "src" into the compiled "dist" website, without changing it
+  eleventyConfig.addPassthroughCopy("src/css");
  
   return {
     dir: {
